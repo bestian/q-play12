f23659f706763c9015498204fe1e8496462b7e0e
Bestian Tang <bestian@gmail.com>
2019-07-05 04:21:03 +0800

    Update Play12.vue

--- src/pages/Play12.vue
+++ src/pages/Play12.vue
@@ -5,7 +5,7 @@
         | {{$t('use_all_four_cards_to_make_12')}}
     .q-pa-md
       .row
-        .col.col-6.col-sm-6(v-for = "c in cards")
+        .col.col-6.col-md-3(v-for = "c in cards")
           q-card.my-card
             q-card-section.bg-primary.text-white
               .text-h4 {{ c }}
@@ -16,7 +16,20 @@ export default {
   name: 'Play12',
   data () {
     return {
-      cards: [3, 4, 2, 1]
+      lastWork: '',
+      slow: true,
+      win: false,
+      wins: 0,
+      maxWins: 0,
+      myNum: 0,
+      target: 12,
+      max: 6,
+      cards: [
+        Math.floor(Math.random() * 6) + 1,
+        Math.floor(Math.random() * 6) + 1,
+        Math.floor(Math.random() * 6) + 1,
+        Math.floor(Math.random() * 6) + 1
+      ]
     }
   }
 }
